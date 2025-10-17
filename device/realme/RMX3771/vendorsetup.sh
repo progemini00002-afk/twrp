14a3b120f4678849e19f51e6967bdd25128224ae
progemini00002-afk <progemini00002@gmail.com>
2025-10-17 10:33:08 +0000

    Fix: Add required filesystem types and remove obsolete lunch combos

--- device/realme/RMX3771/vendorsetup.sh
+++ device/realme/RMX3771/vendorsetup.sh
@@ -5,5 +5,5 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo twrp_RMX3771-eng
-add_lunch_combo twrp_RMX3771-userdebug
+# For TWRP 12.1+, lunch combos are defined in AndroidProducts.mk
+# This file is kept for compatibility but is no longer required
